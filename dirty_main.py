9824768a5ce9937692b97c0de941270d63e3643c
you2zz <yri_vla@mail.ru>
2023-07-28 09:04:10 +0500

    add dirty_main and requirements.txt

---
+++ dirty_main.py
@@ -0,0 +1,12 @@
+from datetime import *
+from application.salary import *
+from application.db.people import *
+
+if __name__ == '__main__':
+
+    print(date.today())
+
+    calculate_salary()
+
+    get_employees()
+
